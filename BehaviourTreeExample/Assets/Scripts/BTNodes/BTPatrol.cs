766b3adf1789637e449e28b0eef1ed157f71d505
Jogchum1 <jogchum1@hotmail.nl>
2022-06-02 18:33:12 +0200

    added patrol script

---
+++ BehaviourTreeExample/Assets/Scripts/BTNodes/BTPatrol.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTPatrol : BTBaseNode
+{
+    public override BTResult Run()
+    {
+        return BTResult.Success;
+    }
+
+    
+}
